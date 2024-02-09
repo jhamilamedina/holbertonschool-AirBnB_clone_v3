130f561e04106430d875628b70f302f2e691ff95
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-09 12:09:48 -0500

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,8 +16,8 @@ def returnstuff():
 @app_views.route('/stats', strict_slashes=False)
 def stuff():
     '''JSON Responses'''
-    todos = {'states': State, 'users': User,'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
+    todos = {'states': State, 'users': User, 'amenities': Amenity, 
+             'cities': City, 'places': Place, 'reviews': Review}
     
     for key in todos:
         todos[key] = storage.count(todos[key])
