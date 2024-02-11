804fa6679f7a0878458be61aaa23095b4f7e60c9
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-11 01:23:45 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-initialize the models package
-"""
+"""create blueprint"""
+from flask import Blueprint
 
-from os import getenv
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-storage.reload()
+if app_views is not None:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.places_reviews import *
+    from api.v1.views.places_amenities import *
