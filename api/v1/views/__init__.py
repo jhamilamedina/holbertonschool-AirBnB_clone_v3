009144f8209836c8ffa96d96f5b1bf2e0674a6a6
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-10 19:23:10 -0500

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,4 @@ from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-"""Esta línea siguiente evitara de errores de importación circulante"""
 from api.v1.views.index import *
