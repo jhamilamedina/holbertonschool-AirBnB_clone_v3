3c0a35188c21cc5dc3e2746cbdfafce6d6046508
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-10 19:18:37 -0500

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 """Nuestra primera respuesta"""
 
-from tkinter import Place
 from flask import jsonify
 from api.v1.views import app_views
 from console import classes
 from models import storage
-from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.city import City
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -23,9 +17,15 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def some_stats():
     """Cuenta todas las clases por tipo."""
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+    tables = {"Amenity": 'amenities',
+              "City": 'cities',
+              "Place": 'places',
+              "Review": 'reviews',
+              "State": 'states',
+              "User": 'users'}
+
+    stats_class = {}
+
+    stats_class = {f'{tables[k]}': storage.count(v)
+                   for k, v in classes.items() if v != BaseModel}
+    return jsonify(stats_class)
