f373c44b7844a9eccd383394a9cc5f57e9ed6289
Maly Medina <jhamilamedina>
2024-02-12 17:02:09 -0800

    Fix: Pycodestyle de user

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,15 @@
 
 from flask import Flask
 from models import storage
+from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
 from flask import jsonify
 
 app = Flask(__name__)
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
