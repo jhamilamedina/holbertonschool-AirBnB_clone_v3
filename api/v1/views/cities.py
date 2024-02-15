a10b6960704f74248a7cc4726e0e4c0d87fc3db6
Maly Medina <jhamilamedina>
2024-02-14 17:14:31 -0800

    Fix: Actualizacion State

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """cities"""
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.city import City
 from models.state import State
